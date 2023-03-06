97628d272c676b161fdec0abe665731544a82f6a
Nicole Dicochea <dicocheanicole@gmail.com>
2023-03-05 19:17:33 -0800

    changed button hover, added form validation to prevent empty input

--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,11 @@ btn.addEventListener('click', () => {
     menu.classList.toggle('hidden');
 })
 
+const form = document.querySelector('#movieForm');
+const inputField = document.querySelector('input[name="movieTitle"]');
+form.addEventListener('submit', (event) => {
+  if (inputField.value === '') {
+    event.preventDefault();
+    alert('Please enter a movie title');
+  }
+});
