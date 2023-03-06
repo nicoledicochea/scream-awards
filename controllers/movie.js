274ba62b410515bb94653929ddb8f9500b293351
Nicole Dicochea <dicocheanicole@gmail.com>
2023-03-05 19:09:13 -0800

    update movie cards

--- controllers/movie.js
+++ controllers/movie.js
@@ -9,29 +9,43 @@ module.exports = {
       const fetch = await import('node-fetch');
       const response = await fetch.default(`https://www.omdbapi.com/?t=${urlMovie}&apikey=b93a8d4e`);
       const data = await response.json();
-      // console.log('data.Poster:', poster);
   
-      const movie = new Movie({
-        movieTitle: movieTitle,
-        likes: 1,
-        poster: data.Poster,
-        plot: data.Plot,
-        released: data.Released,
-        director: data.Director,
-        writer: data.Writer,
-        actors: data.Actors,
-        awards: data.Awards,
-        language: data.Language
-      });
+      const movie = await Movie.findOne({ movieTitle: movieTitle });
+      let update;
+      if (movie) {
+        update = {
+          $inc: { likes: 1 }
+        };
+      } else {
+        update = {
+          $set: { movieTitle: movieTitle,
+                  likes: 1,
+                  poster: data.Poster,
+                  plot: data.Plot,
+                  released: data.Released,
+                  director: data.Director,
+                  writer: data.Writer,
+                  actors: data.Actors,
+                  awards: data.Awards,
+                  language: data.Language
+          }
+        };
+      }
   
-      await movie.save();
+      const options = {
+        upsert: true,
+        new: true,
+        setDefaultsOnInsert: true
+      };
+  
+      await Movie.findOneAndUpdate({ movieTitle: movieTitle }, update, options);
   
       console.log(movieTitle, 'Likes +1');
       if (movieTitle === 'scream' ||
         movieTitle === 'scream 2' ||
         movieTitle === 'scream 3' ||
         movieTitle === 'scream 4' ||
-        movieTitle === 'scream 5' ||
+        movieTitle === 'scream 2022' ||
         movieTitle === 'scream 6') {
         res.redirect(`/movie`);
       } else {
@@ -41,7 +55,64 @@ module.exports = {
       console.error(err);
       next(err);
     }
-  },
+  },  
+
+  // addFave: async (req, res, next) => {
+  //   try {
+  //     const movieTitle = req.body.movieTitle.toLowerCase();
+  //     // const title = movieTitle.split(' ')
+  //     const urlMovie = movieTitle.split(' ').join('+');
+  //     // const urlYear = Number(movieTitle.split(' ')[title.length - 1])
+  //     const fetch = await import('node-fetch');
+  //     const response = await fetch.default(`https://www.omdbapi.com/?t=${urlMovie}&apikey=b93a8d4e`)
+     
+      
+  //     const data = await response.json();
+  //     // console.log('data.Poster:', poster);
+  
+  //     const update = {
+  //       $set: { poster: data.poster,
+  //         poster: data.Poster,
+  //         plot: data.Plot,
+  //         released: data.Released,
+  //         director: data.Director,
+  //         writer: data.Writer,
+  //         actors: data.Actors,
+  //         awards: data.Awards,
+  //         language: data.Language
+  //        },
+  //        $inc: { likes: 1 }
+  //     };
+  
+  //     const options = {
+  //       upsert: true,
+  //       new: true,
+  //       setDefaultsOnInsert: true
+  //     };
+  
+  //     const movie = await Movie.findOneAndUpdate(
+  //       { movieTitle: movieTitle },
+  //       update,
+  //       options
+  //     );
+ 
+  
+  //     console.log(movieTitle, 'Likes +1');
+  //     if (movieTitle === 'scream' ||
+  //       movieTitle === 'scream 2' ||
+  //       movieTitle === 'scream 3' ||
+  //       movieTitle === 'scream 4' ||
+  //       movieTitle === 'scream 5' ||
+  //       movieTitle === 'scream 6') {
+  //       res.redirect(`/movie`);
+  //     } else {
+  //       res.redirect(`/movie/wrong`);
+  //     }
+  //   } catch (err) {
+  //     console.error(err);
+  //     next(err);
+  //   }
+  // },
 
   // addFave: async (req, res) => {
   //   try {
@@ -152,12 +223,12 @@ module.exports = {
         );
         console.log("Vote count updated");
         
-        if(req.params.id === '63f3eca65eef6044c8dd79df' ||
-        req.params.id === '63fd5bb956275c2d100fae2e' ||
-        req.params.id === '63f40038ec8a0c3ccc7a2097' ||
-        req.params.id === '63f4036b7e4eaf1d08ace340' ||
-        req.params.id === '63f5329c8691c63e781c8814' ||
-        req.params.id === '63f532928691c63e781c8811'
+        if(req.params.id === '640538515c11fc54743726d9' ||
+        req.params.id === '6405483d09717339507a39af' ||
+        req.params.id === '6405484209717339507a39b2' ||
+        req.params.id === '6405385b5c11fc54743726e2' ||
+        req.params.id === '640550ce889a24327c66d5aa' ||
+        req.params.id === '640556e01889ed3788635bf2' 
         ) {
           res.redirect(`/movie`)
         } else {
